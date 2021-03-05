9b7b1b993b06d7f8400104b75a0e60b3446ebbbc
karolrobertax3 <karolroberta92@hotmail.com>
2021-03-04 23:02:17 -0300

    Criação das tabelas Venda e VendaItem

--- controle_vendas/admin.py
+++ controle_vendas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HistoricoVendas, HistoricoAtualizacaoPrecos
+from .models import HistoricoVendas, HistoricoAtualizacaoPrecos, Venda, VendaItem
 from controle_estoque.utils import export_as_csv, export_xlsx, salva_criado_por
 
 
@@ -25,3 +25,17 @@ class HistoricoAtualizacaoPrecosAdmin(admin.ModelAdmin):
                    'criado_em', ]
 
     actions = (export_as_csv, export_xlsx)
+
+# @admin.register(Venda)
+# class VendaAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'preco_venda', 'descricao', 'nota_fiscal', 'data_venda', 'status', 'criado_por',]
+#     search_fields = ['id', 'preco_venda', 'descricao', 'nota_fiscal', 'data_venda', 'status', 'criado_por',]
+#     list_filter = ['preco_venda', 'descricao', 'nota_fiscal', 'data_venda', 'status', 'criado_por', ]
+#
+#     actions = (export_as_csv, export_xlsx)
+#
+#     def save_model(self, request, obj, form, change):
+#         salva_criado_por(request, obj)
+
+admin.site.register(Venda)
+admin.site.register(VendaItem)
--- controle_vendas/models.py
+++ controle_vendas/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from controle_usuarios.models import Funcionario
@@ -42,3 +43,66 @@ class HistoricoVendas(models.Model):
         verbose_name = 'Histórico de Venda'
         verbose_name_plural = 'Histórico de Vendas'
         ordering = ['-criado_em']
+
+
+class VendaItem(models.Model):
+    produto = models.ForeignKey("controle_estoque.Produto", on_delete=models.CASCADE)
+    # produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantidade} peças de {self.produto.descricao}"
+
+    def get_total_preco_produto(self):
+        return self.quantidade * self.produto.preco_compra
+
+STATUS_CHOICES = (
+    (1, 'Gerado'),
+    (2, 'Em separação'),
+    (3, 'Enviado'),
+    (4, 'Concluído'),
+)
+
+class Venda(models.Model):
+
+    # fornecedor = models.ForeignKey('controle_estoque.Fornecedor', on_delete=models.CASCADE, related_name='pedidos')
+    produtos = models.ManyToManyField(VendaItem)
+    valor_total_venda = models.DecimalField('Valor Total da Venda', decimal_places=2, max_digits=12, default=0)
+    descricao = models.TextField('Descrição da Venda', max_length=150, blank=True, null=True)
+    nota_fiscal = models.FileField('Nota Fiscal Eletronica', upload_to='controle_pedidos/NFE', blank=True, null=True)
+    data_venda = models.DateTimeField('Venda Realizada Em', auto_now_add=True, editable=True)
+    status = models.SmallIntegerField('Status', choices=STATUS_CHOICES, default=1)
+    criado_por = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    caixa = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='caixa')
+    vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='vendedor')
+    # sub_total = models.DecimalField('Sub Total da Venda', decimal_places=2, max_digits=12, default=0)
+    # desconto = models.DecimalField('Desconto', decimal_places=2, max_digits=12, default=0)
+    forma_pagto = models.CharField("Forma De Pagamento", max_length=30)
+    cpf = models.CharField("CPF", max_length=30)
+
+    # def save(self, *args, **kwargs):
+    #     self.desconto /= 100
+    #     self.valor_total_venda = self.sub_total * self.desconto
+    #     super(Venda, self).save(*args, **kwargs)
+
+
+    class Meta:
+        verbose_name = 'Venda'
+        verbose_name_plural = 'Vendas'
+
+    def __str__(self):
+        return f'{self.id} - {self.data_venda}'
+
+    def calc_total(self):
+        total = 0
+        for order_item in self.produtos.all():
+            total -= order_item.get_total_preco_produto()
+        self.preco_venda = total
+        return self.preco_venda
+
+    def save(self, *args, **kwargs):
+        try:
+            self.calc_total()
+            super(Venda, self).save(*args, **kwargs)
+        except ValueError:
+            super(Venda, self).save(*args, **kwargs)
