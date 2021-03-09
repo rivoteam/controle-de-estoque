03aa4fec1ab481056cddd2e0a3d2bf4c5086d269
Gabrielsuncin <gc.everit@itmss.com.br>
2021-03-08 21:30:06 -0300

    first merge

--- controle_vendas/models.py
+++ controle_vendas/models.py
@@ -3,10 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from controle_usuarios.models import Funcionario
 
-VENDAS_STATUS = (
-    ('Concluída', 'Concluída'),
-    ('Cancelada', 'Cancelada'),
-)
 
 
 class HistoricoAtualizacaoPrecos(models.Model):
@@ -31,7 +27,6 @@ class HistoricoAtualizacaoPrecos(models.Model):
 
 class VendaItem(models.Model):
     produto = models.ForeignKey("controle_estoque.Produto", on_delete=models.CASCADE)
-    # produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.IntegerField(default=1)
 
     def __str__(self):
@@ -40,48 +35,53 @@ class VendaItem(models.Model):
     def get_total_preco_produto(self):
         return self.quantidade * self.produto.preco_compra
 
+
 STATUS_CHOICES = (
-    (1, 'Gerado'),
-    (2, 'Em separação'),
-    (3, 'Enviado'),
-    (4, 'Concluído'),
+    (1, 'Pendente'),
+    (2, 'Cancelada'),
+    (3, 'Concluída'),
 )
 
-class Venda(models.Model):
 
-    # fornecedor = models.ForeignKey('controle_estoque.Fornecedor', on_delete=models.CASCADE, related_name='pedidos')
+PAGAMENTO_CHOICES = (
+    (1, 'Debito'),
+    (2, 'Credito'),
+    (3, 'Dinheiro'),
+    (4, 'PicPay'),
+    (5, 'Pix'),
+)
+
+
+class Venda(models.Model):
     produtos = models.ManyToManyField(VendaItem)
     valor_total_venda = models.DecimalField('Valor Total da Venda', decimal_places=2, max_digits=12, default=0)
     descricao = models.TextField('Descrição da Venda', max_length=150, blank=True, null=True)
     nota_fiscal = models.FileField('Nota Fiscal Eletronica', upload_to='controle_pedidos/NFE', blank=True, null=True)
-    data_venda = models.DateTimeField('Venda Realizada Em', auto_now_add=True, editable=True)
+    criado_em = models.DateTimeField('Venda Realizada Em', auto_now_add=True)
     status = models.SmallIntegerField('Status', choices=STATUS_CHOICES, default=1)
-    criado_por = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    caixa = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='caixa')
-    vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='vendedor')
-    # sub_total = models.DecimalField('Sub Total da Venda', decimal_places=2, max_digits=12, default=0)
-    # desconto = models.DecimalField('Desconto', decimal_places=2, max_digits=12, default=0)
-    forma_pagto = models.CharField("Forma De Pagamento", max_length=30)
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vendas_criadopor',
+                                   editable=False)
+    caixa = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='vendas_caixa',
+                              verbose_name='Operador/Caixa')
+    vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='vendas_vendedor',
+                                 verbose_name="Vendedor")
+    desconto = models.DecimalField('Desconto', decimal_places=2, max_digits=12, default=0,
+                                   help_text="Digite o valor de desconto da venda, exemplo R$ 10,00")
+    forma_pagto = models.SmallIntegerField("Forma De Pagamento", choices=PAGAMENTO_CHOICES)
     cpf = models.CharField("CPF", max_length=30)
-
-    # def save(self, *args, **kwargs):
-    #     self.desconto /= 100
-    #     self.valor_total_venda = self.sub_total * self.desconto
-    #     super(Venda, self).save(*args, **kwargs)
-
 
     class Meta:
         verbose_name = 'Venda'
         verbose_name_plural = 'Vendas'
 
     def __str__(self):
-        return f'{self.id} - {self.data_venda}'
+        return f'Venda {self.id} - {self.criado_em.strftime("%d/%m/%Y - %H:%M:%S")}'
 
     def calc_total(self):
         total = 0
         for order_item in self.produtos.all():
-            total -= order_item.get_total_preco_produto()
-        self.preco_venda = total
+            total += order_item.get_total_preco_produto()
+        self.preco_venda = total - self.desconto
         return self.preco_venda
 
     def save(self, *args, **kwargs):
@@ -92,21 +92,17 @@ class Venda(models.Model):
             super(Venda, self).save(*args, **kwargs)
 
 
-
 class HistoricoVendas(models.Model):
-    vendas = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='vendas')
-    status = models.CharField(max_length=30, choices=VENDAS_STATUS, default='Concluída')
+    vendas = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, default='Concluída')
     valor_total_diario = models.DecimalField(max_digits=6, decimal_places=2)
-    vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='cargo_vendedor')
-    caixa = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='cargo_caixa')
-    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hist_vendas_criado_por',
+    vendedor = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name="historico_vendedor")
+    caixa = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name="historico_caixa")
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name="historico_criado",
                                    editable=False)
     criado_em = models.DateTimeField(auto_now_add=True)
-
 
     class Meta:
         verbose_name = 'Histórico de Venda'
         verbose_name_plural = 'Histórico de Vendas'
         ordering = ['-criado_em']
-
-
