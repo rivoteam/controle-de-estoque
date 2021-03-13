30db6b814b086b12d8b2668030d519695c96e644
Gabrielsuncin <gc.everit@itmss.com.br>
2021-03-12 21:59:15 -0300

    updating detail and list pedido

--- interfaces/urls.py
+++ interfaces/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage, appvendas, lista_pedidos, detalhe_produto
+from .views import homepage, appvendas, lista_pedidos, detalhe_produto, detalhe_pedido
 
 urlpatterns = [
     path('', homepage, name="homepage"),
     path('app-vendas', appvendas, name="appvendas"),
-    path('detalhe/produto/<int:pk>', detalhe_produto, name='detalhe_produto'),
+
+    # Listagem
     path('lista-pedidos', lista_pedidos, name="lista-pedidos"),
+
+    # Detalhes por object
+    path('detalhe-produto/<int:pk>', detalhe_produto, name='detalhe_produto'),
+    path('detalhe-pedido/<int:pk>', detalhe_pedido, name='detalhe_pedido'),
 ]
--- interfaces/views.py
+++ interfaces/views.py
@@ -1,37 +1,47 @@
 from django.shortcuts import render
-
-from controle_estoque.models import Produto
-from core.settings import STATIC_URL
 from controle_estoque.models import Produto
 from core.utils import get_user_profile
 from controle_pedidos.models import PedidoCompra
 
+
 def homepage(request):
     context_user = get_user_profile(request)
-    context = {}
+    context = {
+        "active": "homepage"
+    }
     context.update(context_user)
     return render(request, 'homepage.html', context)
 
 
 def appvendas(request):
     context = {
-        'produtos': Produto.objects.all()
+        'produtos': Produto.objects.all(),
+        "active": "app-vendas"
     }
     context_user = get_user_profile(request)
     context.update(context_user)
-    return render(request, 'app-venda.html', context)
+    return render(request, 'app_venda.html', context)
 
 
 def detalhe_produto(request, pk):
     context = {
-        'produto': Produto.objects.get(pk=pk)
+        'produto': Produto.objects.get(pk=pk),
     }
-    return render(request, 'produto_detalhe.html', context)
+    return render(request, 'detalhe_produto.html', context)
+
 
 def lista_pedidos(request):
     context_user = get_user_profile(request)
     context = {
-        "pedidos": PedidoCompra.objects.all()
+        "pedidos": PedidoCompra.objects.all(),
+        "active": "lista-pedidos"
     }
     context.update(context_user)
     return render(request, 'pedidos.html', context)
+
+
+def detalhe_pedido(request, pk):
+    context = {
+        'produto': PedidoCompra.objects.get(pk=pk),
+    }
+    return render(request, 'modal_detalhe_pedido.html', context)
