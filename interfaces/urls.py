f42e6bc160ab963f2ef9cb51164705dbb25d17d5
Ermirio Alves Bonfim <ermirio.contato@gmail.com>
2021-03-12 21:08:02 -0300

    'criacação-interface-pedidos'

--- interfaces/urls.py
+++ interfaces/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage, appvendas
+from .views import homepage, appvendas, lista_pedidos
 
 urlpatterns = [
     path('', homepage, name="homepage"),
     path('app-vendas', appvendas, name="appvendas"),
+    path('lista-pedidos', lista_pedidos, name="lista-pedidos"),
 ]
--- interfaces/views.py
+++ interfaces/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from core.settings import STATIC_URL
 from controle_estoque.models import Produto
 from core.utils import get_user_profile
-
+from controle_pedidos.models import PedidoCompra
 
 def homepage(request):
     context_user = get_user_profile(request)
@@ -18,3 +18,12 @@ def appvendas(request):
     context_user = get_user_profile(request)
     context.update(context_user)
     return render(request, 'app-venda.html', context)
+
+
+def lista_pedidos(request):
+    context_user = get_user_profile(request)
+    context = {
+        "pedidos": PedidoCompra.objects.all()
+    }
+    context.update(context_user)
+    return render(request, 'pedidos.html', context)
