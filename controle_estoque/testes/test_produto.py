126734af7038a75d8a37c642c6efbb46a0d6f108
Amauri <amaurirg@terra.com.br>
2021-03-24 01:07:34 -0300

    some tests with invalid data

--- controle_estoque/testes/test_produto.py
+++ controle_estoque/testes/test_produto.py
@@ -1,3 +1,4 @@
+from django.db.utils import IntegrityError
 from controle_estoque.models import Produto, HistoricoAtualizacaoPrecos
 from .EstoqueTest import EstoqueTest
 
@@ -9,7 +10,7 @@ class ProdutoTestesDadosValidos(EstoqueTest):
         self.novo_produto.save()
         produto_salvo = Produto.objects.get(ean=self.novo_produto.ean)
         historico = HistoricoAtualizacaoPrecos.objects.filter(produto=produto_salvo).first()
-        self.assertEqual(None, produto_salvo.motivo_alteracao_preco)
+        self.assertIsNone(produto_salvo.motivo_alteracao_preco)
         self.assertEqual("Novo produto", historico.motivo_alteracao_preco)
 
     def test_atualiza_preco_do_produto(self):
@@ -22,5 +23,20 @@ class ProdutoTestesDadosValidos(EstoqueTest):
         produto_salvo.save()
         produto_alterado = Produto.objects.get(ean=self.novo_produto.ean)
         historico = HistoricoAtualizacaoPrecos.objects.filter(produto=produto_alterado).first()
-        self.assertEqual(None, produto_alterado.motivo_alteracao_preco)
+        self.assertIsNone(produto_alterado.motivo_alteracao_preco)
         self.assertEqual("Reajuste", historico.motivo_alteracao_preco)
+
+
+class ProdutoTestesDadosInValidos(EstoqueTest):
+    """ Testes com dados inválidos informados """
+    def test_cria_novo_produto_sem_preco_venda(self):
+        """ Não deve criar um novo produto com os dados do setUp() """
+        with self.assertRaises((IntegrityError, TypeError)):
+            self.novo_produto.genero = None
+            self.novo_produto.save()
+
+    def test_cria_novo_produto_sem_criado_por(self):
+        """ Não deve criar um novo produto com os dados do setUp() """
+        with self.assertRaises(IntegrityError):
+            self.novo_produto.criado_por = None
+            self.novo_produto.save()
