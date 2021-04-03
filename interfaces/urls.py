2e5e43deb4a60804e1e2874b3859c0b72851a6ef
karolrobertax3 <karolroberta92@hotmail.com>
2021-04-03 00:14:02 -0300

    Criado modal delete/remove

--- interfaces/urls.py
+++ interfaces/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage, appvendas, lista_pedidos, detalhe_produto, detalhe_pedido, lista_produtos, user_logout
+from .views import homepage, appvendas, lista_pedidos, detalhe_produto, detalhe_pedido, lista_produtos, user_logout, \
+    cria_produto, atualiza_produto, remove_produto, modal_remove_produto, lista_vendas
 
 urlpatterns = [
     path('', homepage, name="homepage"),
@@ -10,8 +11,18 @@ urlpatterns = [
     # Listagem
     path('lista-pedidos', lista_pedidos, name="lista-pedidos"),
     path('lista-produtos', lista_produtos, name="lista-produtos"),
+    path('lista-vendas', lista_vendas, name="lista-vendas"),
+
+    # Criação
+    path("cria-produto", cria_produto, name="cria-produto"),
+    path("atualiza-produto/<int:pk>", atualiza_produto, name="atualiza-produto"),
 
     # Detalhes por object
     path('detalhe-produto/<int:pk>', detalhe_produto, name='detalhe_produto'),
     path('detalhe-pedido/<int:pk>', detalhe_pedido, name='detalhe_pedido'),
+
+    # Remoção
+    path('modal-remove-produto/<int:pk>', modal_remove_produto, name='modal-remove-produto'),
+    path("remove-produto/<int:pk>", remove_produto, name="remove-produto"),
+
 ]
--- interfaces/views.py
+++ interfaces/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
 from controle_estoque.models import Produto
 from controle_pedidos.models import PedidoCompra
+from controle_vendas.models import Venda
 
 
 @login_required()
@@ -59,3 +63,64 @@ def detalhe_pedido(request, pk):
         'produtos': produtos,
     }
     return render(request, 'modal_detalhe_pedido.html', context)
+
+
+@login_required()
+def lista_vendas(request):
+    context = {
+        "vendas": Venda.objects.all(),
+        "active": "lista-vendas"
+    }
+    return render(request, 'vendas.html', context)
+
+
+# @login_required()
+class CriaProduto(CreateView):
+    template_name = "cria-produto.html"
+    model = Produto
+    fields = "__all__"
+    context_object_name = "form"
+    success_url = reverse_lazy("lista-produtos")
+
+    def form_valid(self, form):
+        form.instance.criado_por = self.request.user
+        return super().form_valid(form)
+
+# @login_required()
+class AtualizaProduto(UpdateView):
+    template_name = "cria-produto.html"
+    model = Produto
+    fields = "__all__"
+    success_url = reverse_lazy("lista-produtos")
+
+    def form_valid(self, form):
+        form.instance.atualizado_por = self.request.user
+        return super().form_valid(form)
+
+# @login_required()
+# def remove_produto(request, pk):
+#     produto = Produto.objects.get(pk=pk)
+#     produto.delete()
+#     return HttpResponseRedirect("/lista-produtos")
+
+
+cria_produto = CriaProduto.as_view()
+atualiza_produto = AtualizaProduto.as_view()
+
+
+@login_required()
+def modal_remove_produto(request, pk):
+    produto = get_object_or_404(Produto, pk=pk)
+    context = {
+        'produto': produto,
+    }
+    return render(request, 'modal_remove_produto.html', context)
+
+
+@login_required()
+def remove_produto(request, pk):
+    produto = Produto.objects.get(pk=pk)
+    produto.ativo = False
+    produto.save()
+    return redirect(reverse('lista-produtos'))
+
