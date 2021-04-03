62a7368e0f46a9e0f4af13479ed2dce1c8a4a8c1
Thais <thais.provazi@gmail.com>
2021-04-03 13:33:13 -0300

    view e pagina modal

--- interfaces/urls.py
+++ interfaces/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage, appvendas, lista_pedidos, detalhe_produto, detalhe_pedido, lista_produtos, user_logout
+from .views import homepage, appvendas, lista_pedidos, detalhe_produto, detalhe_pedido, lista_produtos, user_logout, \
+    lista_vendas, detalhe_venda, modal_cria_pedido, modal_atualiza_pedido, modal_remove_pedido
 
 urlpatterns = [
     path('', homepage, name="homepage"),
@@ -10,8 +11,16 @@ urlpatterns = [
     # Listagem
     path('lista-pedidos', lista_pedidos, name="lista-pedidos"),
     path('lista-produtos', lista_produtos, name="lista-produtos"),
+    path('lista-vendas', lista_vendas, name="lista-vendas"),
 
     # Detalhes por object
     path('detalhe-produto/<int:pk>', detalhe_produto, name='detalhe_produto'),
     path('detalhe-pedido/<int:pk>', detalhe_pedido, name='detalhe_pedido'),
+    path('detalhe-venda/<int:pk>', detalhe_venda, name='detalhe_venda'),
+
+    # Criação
+    # path('cria-pedido', cria_pedido, name='cria-pedido'),
+    path('modal-cria-pedido', modal_cria_pedido, name='modal-cria-pedido'),
+    path('modal-atualiza-pedido', modal_atualiza_pedido, name='modal-atualiza-pedido'),
+    path('modal-remove-pedido', modal_remove_pedido, name='modal-remove-pedido'),
 ]
--- interfaces/views.py
+++ interfaces/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
 from controle_estoque.models import Produto
 from controle_pedidos.models import PedidoCompra
+from controle_vendas.models import Venda
+from interfaces.forms import PedidoForm
 
 
 @login_required()
@@ -59,3 +64,71 @@ def detalhe_pedido(request, pk):
         'produtos': produtos,
     }
     return render(request, 'modal_detalhe_pedido.html', context)
+
+
+@login_required()
+def lista_vendas(request):
+    context = {
+        "vendas": Venda.objects.all(),
+        "active": "lista-vendas"
+    }
+    return render(request, 'vendas.html', context)
+
+
+@login_required()
+def detalhe_venda(request, pk):
+    venda = Venda.objects.get(pk=pk)
+    produtos = venda.carrinhovenda_set.all()
+    context = {
+        'venda': venda,
+        'produtos': produtos,
+    }
+    return render(request, 'modal_detalhe_venda.html', context)
+
+
+#@login_required()
+#def modal_cria_pedido(request):
+#    pass
+
+
+class ModalCriaPedido(CreateView):
+    template_name = "modal_cria_pedido.html"
+    model = PedidoCompra
+    fields = "__all__"
+    context_object_name = "form"
+    success_url = reverse_lazy("lista-pedidos")
+
+    def form_valid(self, form):
+        form.instance.criado_por = self.request.user
+        return super().form_valid(form)
+#
+# def cria_pedido(request):
+#
+#
+#
+modal_cria_pedido = ModalCriaPedido.as_view()
+
+
+# def modal_cria_pedido(request):
+#     form = PedidoForm(request.POST or None, request.FILES or None)
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'modal_cria_pedido.html', context)
+#
+#
+# def cria_pedido(request):
+#     form = PedidoForm(request.POST, request.FILES)
+#     if form.is_valid():
+#         form.save()
+#         return redirect(reverse('lista-pedidos'))
+#         # return HttpResponseRedirect(f'/lista-pedidos')
+
+@login_required()
+def modal_atualiza_pedido(request):
+    pass
+
+
+@login_required()
+def modal_remove_pedido(request):
+    pass
