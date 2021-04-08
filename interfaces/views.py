ddc5c37ebce68189bc1eb6659e396c399744cb61
Gabriel Suncin <suncin27@gmail.com>
2021-04-07 23:19:03 -0300

    updating dashboard chart

--- interfaces/views.py
+++ interfaces/views.py
@@ -5,10 +5,14 @@ from django.shortcuts import render
 from controle_estoque.models import Produto
 from controle_pedidos.models import PedidoCompra
 from controle_vendas.models import Venda
+import datetime
 
 
 @login_required()
 def homepage(request):
+    """
+    Cards Front
+    """
     qtd_produtos = Produto.objects.filter(ativo=True).count()
     qtd_produtos_limite_alerta_min = Produto.objects.filter(ativo=True, limite_alerta_min=False).count()
     qtd_vendas = Venda.objects.filter(ativo=True).count()
@@ -17,9 +21,26 @@ def homepage(request):
         'active': 'homepage',
         'qtd_produtos': qtd_produtos,
         'qtd_vendas': qtd_vendas,
-        'qtd_produtos_limite_alerta_min':qtd_produtos_limite_alerta_min
+        'qtd_produtos_limite_alerta_min': qtd_produtos_limite_alerta_min
     }
+
+    """
+    Vendas por mes
+    """
+    today = datetime.datetime.today()
+    first_day_in_month = today.replace(day=1)
+    for i in range(12):
+        vendido_no_mes = 0
+        mes = first_day_in_month.strftime("%Y-%m")
+        vendas = Venda.objects.filter(criado_em__startswith=mes)
+        context[f'mes_{i}'] = mes
+        for venda in vendas:
+            vendido_no_mes += int(venda.calc_faturamento())
+        context[f'registro_{i}'] = vendido_no_mes
+        last_month = first_day_in_month - datetime.timedelta(days=1)
+        first_day_in_month = last_month.replace(day=1)
     return render(request, 'homepage.html', context)
+
 
 @login_required()
 def user_logout(request):
@@ -43,6 +64,7 @@ def lista_produtos(request):
         "active": "lista-produtos"
     }
     return render(request, 'produtos.html', context)
+
 
 @login_required()
 def detalhe_produto(request, pk):
