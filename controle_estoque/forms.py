17d7368dde8961f4a62713a506a6a6bdb80520fb
Leo5677 <leonardovalensoela567567@gmail.com>
2021-06-03 09:49:03 -0300

    Corrigindo valores negativos ou valor zerado em: preco compra, venda e total pecas

--- controle_estoque/forms.py
+++ controle_estoque/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-
 from controle_estoque.models import Produto
 
 
 class ProdutoForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProdutoForm, self).__init__(*args, **kwargs)
+        self.fields['preco_compra'] = forms.DecimalField(max_digits=6, decimal_places=2, min_value=1.00)
+        self.fields['preco_venda'] = forms.DecimalField(max_digits=6, decimal_places=2, min_value=1.00)
+        self.fields['total_pecas'] = forms.IntegerField(min_value=1)
+
     class Meta:
         model = Produto
         fields = ['descricao', 'genero', 'categoria', 'subcategoria', 'tamanho', 'cor', 'min_pecas', 'alerta_min',
